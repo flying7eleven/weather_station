2997aea3641ea15cabf1bf137913ae1f2c65cce5
Tim Hütz <tim@huetz.biz>
2019-05-21 19:38:50 +0200

    :tada: initial version of the firmware

---
+++ weather_station.ino
@@ -0,0 +1,63 @@
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 1
+#define VERSION_PATCH 0
+
+#define WIFI_HOST ""
+#define WIFI_SSID ""
+#define WIFI_PASS ""
+
+#include <ESP8266WiFi.h>
+
+void(* resetFunc) (void) = 0;
+
+void indicateStillConnecting() {
+  pinMode(BUILTIN_LED, OUTPUT);
+
+  digitalWrite(BUILTIN_LED, LOW);
+  delay(100);
+  digitalWrite(BUILTIN_LED, HIGH);
+  delay(100);
+}
+
+void indicateConnected() {
+  pinMode(BUILTIN_LED, OUTPUT);
+
+  digitalWrite(BUILTIN_LED, LOW);
+  delay(2000);
+  digitalWrite(BUILTIN_LED, HIGH);
+}
+
+void setup() {
+  int connectionTries = 0;
+
+  // setup the serial interface with a specified baud rate
+  Serial.begin(115200);
+
+  // just print a simple header
+  Serial.println();
+  Serial.printf("Solar Powered Weather Station %d.%d.%d - Written by Tim Huetz. All rights reserved.\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
+  Serial.printf("================================================================================");
+  Serial.println();
+
+  // try to connect to the wifi
+  WiFi.hostname(WIFI_HOST);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED)  {
+    connectionTries++;
+    indicateStillConnecting();
+    delay(500);
+    if (connectionTries > 20) {
+      Serial.println();
+      Serial.printf("Could not connect after %d tries, resetting and starting from the beginning...", connectionTries);
+      resetFunc();
+    }
+  }
+  indicateConnected();
+}
+
+void loop() {
+  Serial.printf("Chip ID = %08X\n", ESP.getChipId());
+  Serial.println("");
+  WiFi.printDiag(Serial);
+  delay(5000);
+}
